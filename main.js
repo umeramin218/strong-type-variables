b0f82e38f4e594dbf83bc76d3a31bddbbe513bdd
umeramin218 <160252422+umeramin218@users.noreply.github.com>
2024-05-01 14:42:41 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var userName = "umer amin";
+console.log(userName);
+var age = 23;
+console.log(age);
+var isStudent = false;
+console.log(isStudent);
+var randomThings = "ab";
+console.log(randomThings);
---
+++ main.ts
@@ -0,0 +1,18 @@
+let userName: string= "umer amin";
+console.log(userName);
+ 
+let age: number = 23
+console.log(age);
+
+let isStudent:boolean = false;
+console.log(isStudent);
+
+let randomThings: any= "ab";
+console.log(randomThings);
+
+
+
+
+
+
+
